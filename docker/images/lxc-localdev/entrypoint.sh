448b0ed50709f80e07ded6a4432ad3402e022e80
Gregory Amerson <gregory.amerson@liferay.com>
2022-09-22 00:33:24 -0500

    add static extension support and update workspace version

--- docker/images/lxc-localdev/entrypoint.sh
+++ docker/images/lxc-localdev/entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+export LXC_LOCALDEV_REPO=/repo
 export KUBECONFIG=$(k3d kubeconfig write lxc-localdev 2>/dev/null)
 export TILT_HOST=0.0.0.0
 
--- k8s/endpoint/yaml.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-IP=$(kubectl get cm coredns --namespace kube-system -o jsonpath='{.data.NodeHosts}' | grep host.k3d.internal | awk '{print $1}')
-
-ytt \
-	-f k8s/endpoint \
-	--data-value "dockerHostAddress=${IP}" \
-	--data-value "virtualInstanceId=${1}"
