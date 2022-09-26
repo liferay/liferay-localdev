34f2dc73e87bfc69c74e3646f46fb65e4fc57cd0
Gregory Amerson <gregory.amerson@liferay.com>
2022-09-25 23:59:13 -0500

    remove LIFERAY_CACHE requirement

--- docker/bin/localdev-down.sh
+++ docker/bin/localdev-down.sh
@@ -7,17 +7,11 @@ if [ -z "$LOCALDEV_REPO" ]; then
   exit 1
 fi
 
-if [ -z "$LIFERAY_CACHE" ]; then
-  echo "Must specify LIFERAY_CACHE env var"
-  exit 1
-fi
-
 docker \
   run \
   --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $LOCALDEV_REPO:/repo \
-  -v $LIFERAY_CACHE:/root/.liferay/ \
   -v $(pwd):/workspace/client-extensions \
   localdev \
   tilt down -f /repo/tilt/Tiltfile
