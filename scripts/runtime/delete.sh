940efd6f8ec0a65d74dfa3c7368592c1d30c21ec
Raymond Augé <raymond.auge@liferay.com>
2022-10-04 13:39:29 -0400

    rename the container

--- scripts/runtime/delete.sh
+++ scripts/runtime/delete.sh
@@ -4,6 +4,6 @@ set -e
 
 k3d cluster delete localdev
 
-docker container rm -f dxp-server localdev-dnsmasq localdev-up >/dev/null 2>&1
+docker container rm -f dxp-server localdev-dnsmasq localdev-extension-runtime >/dev/null 2>&1
 
 echo "'localdev' runtime environment deleted."
--- scripts/runtime/stop.sh
+++ scripts/runtime/stop.sh
@@ -6,6 +6,6 @@ k3d cluster stop localdev
 
 # Ignore the result of the following command
 set +e
-RS=$(docker container stop dxp-server localdev-dnsmasq localdev-up >/dev/null 2>&1)
+RS=$(docker container stop dxp-server localdev-dnsmasq localdev-extension-runtime >/dev/null 2>&1)
 
 echo "'localdev' runtime environment stopped."
