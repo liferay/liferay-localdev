7cdf735f9a2f975d53077dc92fc91f9b61488d1b
Gregory Amerson <gregory.amerson@liferay.com>
2022-10-24 13:39:43 -0500

    add supporting script for `liferay runtime kubeconfig` cmd

---
+++ scripts/runtime/kubeconfig.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+KUBECONFIG="${KUBECONFIG:-/var/run/.kube/config}"
+
+k3d \
+  kubeconfig merge localdev \
+  --kubeconfig-merge-default \
+  --kubeconfig-switch-context 1> /dev/null
+
+echo "Successfully merge localdev config and set default context into {HOME}/.kube/config"
