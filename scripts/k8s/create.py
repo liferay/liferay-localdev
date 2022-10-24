4cd8310d41fc6752996024ce700a80ef3d1d9ede
Gregory Amerson <gregory.amerson@liferay.com>
2022-10-23 23:55:05 -0500

    rename to create

---
+++ scripts/k8s/create.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import generate
+import os
+import uuid
+
+tmp_filename="/tmp/create-%s.yaml" % uuid.uuid1()
+
+with open(tmp_filename, 'w') as f:
+  f.write(generate.generate_yaml())
+applied_yaml=os.popen("kubectl create -oyaml -f %s" % tmp_filename).read()
+os.remove(tmp_filename)
+
+print(applied_yaml)
--- scripts/k8s/delete.py
+++ scripts/k8s/delete.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 import generate
 import os
-
-import generate
-import os
 import uuid
 
 tmp_filename="/tmp/delete-%s.yaml" % uuid.uuid1()
 
-def delete_yaml(yaml):
-  with open(tmp_filename, 'w') as f:
-    f.write(yaml)
-  os.popen("kubectl delete --ignore-not-found --wait -f %s" % tmp_filename).read()
-  os.remove(tmp_filename)
-
-delete_yaml(generate.generate_yaml())
+with open(tmp_filename, 'w') as f:
+  f.write(generate.generate_yaml())
+os.popen("kubectl delete --ignore-not-found --wait -f %s" % tmp_filename).read()
+os.remove(tmp_filename)
