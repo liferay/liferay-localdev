69d587b65b18099c2df88c0a76809402ca354033
Gregory Amerson <gregory.amerson@liferay.com>
2022-10-27 01:18:01 -0500

    format

--- scripts/ext/create.py
+++ scripts/ext/create.py
@@ -5,7 +5,9 @@ import os
 import pathlib
 import shutil
 
-projects_base_path = os.environ.get("PROJECTS_BASE_PATH", "/workspace/client-extensions/")
+projects_base_path = os.environ.get(
+    "PROJECTS_BASE_PATH", "/workspace/client-extensions/"
+)
 templates_base_path = os.environ.get("TEMPLATES_BASE_PATH", "/repo/templates/")
 
 parser = argparse.ArgumentParser()
@@ -26,26 +28,26 @@ shutil.copytree(template_path, project_path)
 
 for root, dirs, files in os.walk(project_path):
     for d in dirs:
-      dirpath = os.path.join(root, d)
-      for key in template_args.keys():
-          newdirpath = dirpath.replace("${%s}" % key, template_args[key])
-          newdirparentpath = pathlib.Path(newdirpath).parent
-          pathlib.Path.mkdir(newdirparentpath, parents=True, exist_ok=True)
-          os.rename(dirpath, newdirpath)
-          dirpath = newdirpath
+        dirpath = os.path.join(root, d)
+        for key in template_args.keys():
+            newdirpath = dirpath.replace("${%s}" % key, template_args[key])
+            newdirparentpath = pathlib.Path(newdirpath).parent
+            pathlib.Path.mkdir(newdirparentpath, parents=True, exist_ok=True)
+            os.rename(dirpath, newdirpath)
+            dirpath = newdirpath
 
 for root, dirs, files in os.walk(project_path):
     for f in files:
         filepath = os.path.join(root, f)
         try:
-          with open(filepath) as r:
-              text = r.read()
-              for key in template_args.keys():
-                  text = text.replace("${%s}" % key, template_args[key])
-          with open(filepath, "w") as w:
-              w.write(text)
+            with open(filepath) as r:
+                text = r.read()
+                for key in template_args.keys():
+                    text = text.replace("${%s}" % key, template_args[key])
+            with open(filepath, "w") as w:
+                w.write(text)
         except UnicodeDecodeError:
-          pass
+            pass
         for key in template_args.keys():
             newfilepath = filepath.replace("${%s}" % key, template_args[key])
             newfileparentpath = pathlib.Path(newfilepath).parent
