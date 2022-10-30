8311c9ba4507bdb8fc8ea66a8f45cc75a9a6fc6c
Gregory Amerson <gregory.amerson@liferay.com>
2022-10-29 22:24:04 -0500

    call cli with --noprompt

--- tests/functional/test-create-projects-workspace.sh
+++ tests/functional/test-create-projects-workspace.sh
@@ -35,34 +35,20 @@ mkdir -p $BASE_PATH
 export WORKSPACE_BASE_PATH="$BASE_PATH"
 export BUILD_PROJECTS="false"
 
-../integration/test-create-projects.sh
+$CLI ext create \
+  -v \
+  --noprompt \
+  -- \
+  --resource-path=template/configuration \
+  --workspace-path=coupon-configuration \
+  --args=id=coupon-configuration-import \
+  --args=name="Coupon Configuration Import"
 
-$CLI ext start -d $BASE_PATH
-
-FOUND_LOCALDEV_SERVER=0
-
-until [ "$FOUND_LOCALDEV_SERVER" == "1" ]; do
-  sleep 5
-  FOUND_LOCALDEV_SERVER=$(docker ps | grep localdev-extension-runtime | wc -l)
-  echo "FOUND_LOCALDEV_SERVER=${FOUND_LOCALDEV_SERVER}"
-done
-
-FOUND_EXT_PROVISION_CONFIG_MAPS=0
-
-until [ "$FOUND_EXT_PROVISION_CONFIG_MAPS" == "10" ]; do
-  sleep 5
-  FOUND_EXT_PROVISION_CONFIG_MAPS=$(docker exec -i localdev-extension-runtime /entrypoint.sh kubectl get cm | grep ext-provision-metadata | wc -l | xargs)
-  echo "FOUND_EXT_PROVISION_CONFIG_MAPS=${FOUND_EXT_PROVISION_CONFIG_MAPS}"
-  docker logs -n 100 localdev-extension-runtime
-done
-
-FOUND_EXT_INIT_CONFIG_MAPS=0
-
-until [ "$FOUND_EXT_INIT_CONFIG_MAPS" == "3" ]; do
-  sleep 5
-  FOUND_EXT_INIT_CONFIG_MAPS=$(docker exec -i localdev-extension-runtime /entrypoint.sh kubectl get cm | grep ext-init-metadata | wc -l | xargs)
-  echo "FOUND_EXT_INIT_CONFIG_MAPS=${FOUND_EXT_INIT_CONFIG_MAPS}"
-  docker logs -n 100 localdev-extension-runtime
-done
-
-$CLI runtime delete
+$CLI ext create \
+  -v \
+  --noprompt \
+  -- \
+  --resource-path=template/service-springboot \
+  --workspace-path=coupon-service \
+  --args=package="com.company.service"\
+  --args=packagePath="com/company/service"
