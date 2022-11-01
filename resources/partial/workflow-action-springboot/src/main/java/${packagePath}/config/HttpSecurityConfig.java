91c3a1ffa7470af8808691346578749b5d1380ed
Raymond Augé <raymond.auge@liferay.com>
2022-11-01 15:01:05 -0400

    workflow action partial

---
+++ resources/partial/workflow-action-springboot/src/main/java/${packagePath}/config/HttpSecurityConfig.java
@@ -0,0 +1,104 @@
+package ${package}.config;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
+import com.nimbusds.jose.proc.JWSAlgorithmFamilyJWSKeySelector;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+
+@EnableWebSecurity
+@Configuration
+public class HttpSecurityConfig {
+
+	@Value("${com.liferay.lxc.dxp.mainDomain}")
+	private String _mainDomain;
+
+	@Bean
+	public String mainDomain() {
+		return _mainDomain;
+	}
+
+	@Value("${com.liferay.lxc.dxp.domains}")
+	private String _lxcDXPDomains;
+
+	@Bean
+	public List<String> allowedOrigins() {
+		return Stream.of(
+			_lxcDXPDomains.split("\\s*\n\\s*")
+		).map(
+			String::trim
+		).map(
+			"https://"::concat
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(allowedOrigins());
+		configuration.setAllowedMethods(
+			Arrays.asList("DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"));
+		configuration.setAllowedHeaders(
+			Arrays.asList("Authorization", "Content-Type"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder(@Value("${${id}-user-agent.oauth2.jwks.uri}") String jwkSetUrl) throws Exception {
+		JWSKeySelector<SecurityContext> jwsKeySelector =
+			JWSAlgorithmFamilyJWSKeySelector.fromJWKSetURL(new URL(jwkSetUrl));
+
+		DefaultJWTProcessor<SecurityContext> jwtProcessor =
+			new DefaultJWTProcessor<>();
+		jwtProcessor.setJWSKeySelector(jwsKeySelector);
+		jwtProcessor.setJWSTypeVerifier(
+			new DefaultJOSEObjectTypeVerifier<>(new JOSEObjectType("at+jwt")));
+
+		return new NimbusJwtDecoder(jwtProcessor);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http,
+			Collection<Consumer<HttpSecurity>> adapters) throws Exception {
+		return http.cors(
+		).and(
+		).csrf(
+		).disable(
+		).sessionManagement(
+		).sessionCreationPolicy(
+			SessionCreationPolicy.STATELESS
+		).and(
+		).authorizeHttpRequests(
+				authorize -> authorize.anyRequest().authenticated()
+			).oauth2ResourceServer(
+					OAuth2ResourceServerConfigurer::jwt).build();
+	}
+
+}
