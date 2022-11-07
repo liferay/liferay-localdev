4f3ff4e1146edbc57b487c57427a8b9d487ee515
Raymond Augé <raymond.auge@liferay.com>
2022-11-07 13:29:31 -0500

    chown /var/run/user to eliminate "Writing Kubernetes config: could not create any of the following paths: /run/user/502/tilt-dev/tilt-default/cluster" error

--- docker/images/localdev-server/entrypoint.sh
+++ docker/images/localdev-server/entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 sudo chown "$LOCALDEV_USER" /var/run/docker.sock
+sudo chown -R "$LOCALDEV_USER" /var/run/user
 
 export LOCALDEV_REPO=${LOCALDEV_REPO:-/repo}
 export KUBECONFIG=${KUBECONFIG:-$(k3d kubeconfig write localdev 2>/dev/null)}
