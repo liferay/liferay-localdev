557a4aa25849a319aaf99d196714b680fd738913
Gregory Amerson <gregory.amerson@liferay.com>
2022-11-06 20:30:36 -0600

    change owner of docker.sock to localdev

--- docker/images/localdev-server/entrypoint.sh
+++ docker/images/localdev-server/entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+sudo chown localdev /var/run/docker.sock
+
 export LOCALDEV_REPO=${LOCALDEV_REPO:-/repo}
 export KUBECONFIG=${KUBECONFIG:-$(k3d kubeconfig write localdev 2>/dev/null)}
 export TILT_HOST=0.0.0.0
