4e064b76ecbef070a353ac1721fff724a384b4a1
Raymond Augé <raymond.auge@liferay.com>
2023-03-13 15:47:45 -0400

    where applicable read details from ${projectDir}/build/LCP.json otherwise use ${projectDir}/LCP.json

--- scripts/k8s/create.py
+++ scripts/k8s/create.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import ytt
 import os
-import uuid
 
-tmpfile = "/tmp/create-%s.yaml" % uuid.uuid1()
+name = os.environ.get("NAME")
+
+tmpfile = "/tmp/.%s.yaml" % name
 
 with open(tmpfile, "w") as f:
     f.write(ytt.generate_workload_yaml())
+
 applied_yaml = os.popen("kubectl create -oyaml -f %s" % tmpfile).read()
-os.remove(tmpfile)
 
 print(applied_yaml)
--- scripts/k8s/delete.py
+++ scripts/k8s/delete.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import ytt
 import os
-import uuid
 
-tmpfile = "/tmp/delete-%s.yaml" % uuid.uuid1()
+name = os.environ.get("NAME")
 
-with open(tmpfile, "w") as f:
-    f.write(ytt.generate_workload_yaml())
-os.popen("kubectl delete --ignore-not-found --wait -f %s" % tmpfile).read()
-os.remove(tmpfile)
+tmpfile = "/tmp/.%s.yaml" % name
+
+if os.path.exists(tmpfile):
+    os.popen("kubectl delete --ignore-not-found --wait -f %s" % tmpfile).read()
+    os.remove(tmpfile)
+    os.remove("/tmp/.%s.yttc" % name)
