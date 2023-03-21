37cebe57f63e6da9f06c1767ee5c5d529455c45c
Raymond Augé <raymond.auge@liferay.com>
2023-03-21 18:01:31 -0400

    verbose output so we don't have to dump logs

--- tests/functional/test-mysql-volume.sh
+++ tests/functional/test-mysql-volume.sh
@@ -49,7 +49,7 @@ $CLI ext create \
 
 cp ${LOCALDEV_REPO}/resources/tilt/Tiltfile.mysql ${WORKSPACE_BASE_PATH}/
 
-$CLI ext start -d ${WORKSPACE_BASE_PATH} &
+$CLI ext start -v -d ${WORKSPACE_BASE_PATH} &
 
 FOUND_LOCALDEV_SERVER=0
 
@@ -73,7 +73,6 @@ until [ "$FOUND_EXT_PROVISION_CONFIG_MAPS" == "1" ]; do
 	sleep 5
 	FOUND_EXT_PROVISION_CONFIG_MAPS=$(docker exec -i localdev-extension-runtime /entrypoint.sh kubectl get cm | grep ext-provision-metadata | wc -l | xargs)
 	echo "FOUND_EXT_PROVISION_CONFIG_MAPS=${FOUND_EXT_PROVISION_CONFIG_MAPS}"
-	docker logs -n 50 localdev-extension-runtime
 done
 
 $CLI ext stop -v
