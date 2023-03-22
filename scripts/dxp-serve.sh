a695bb98f1ea141eed7e72b1549179696a183b8d
Raymond Augé <raymond.auge@liferay.com>
2023-03-22 14:52:10 -0400

    cleanup shell debug flags

--- scripts/dxp-serve.sh
+++ scripts/dxp-serve.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 IMAGE=dxp-server
 
--- scripts/dxp-stop.sh
+++ scripts/dxp-stop.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-set -ex
-
-set +e
 RS=$(docker container stop dxp-server localdev-dnsmasq >/dev/null 2>&1)
 
 echo "'dxp-server' 'localdev-dnsmasq' stopped."
