52fb3e83218bba7e6f7cf244b0f470225a081dc8
Raymond Augé <raymond.auge@liferay.com>
2023-05-04 15:18:54 -0400

    fix permissions of docker socket mount path

--- docker/images/localdev-server/entrypoint.sh
+++ docker/images/localdev-server/entrypoint.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-sudo chown "$LOCALDEV_USER" /var/run/docker.sock
-sudo chown -R "$LOCALDEV_USER" /var/run/user
-
 export LOCALDEV_REPO=${LOCALDEV_REPO:-/repo}
 export KUBECONFIG=${KUBECONFIG:-$(k3d kubeconfig write localdev 2>/dev/null)}
 export TILT_HOST=0.0.0.0
